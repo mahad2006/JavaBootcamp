a9d9fc91c9f6a5c6474fffeb5c753db41379bb39
Shaikh Mahad <codewithmahad+github@gmail.com>
2025-12-23 16:39:07 +0500

    "Lesson 1: structure and printing"

---
+++ src/session1_basics/HelloJava.java
@@ -0,0 +1,21 @@
+package session1_basics;
+
+public class HelloJava {
+    static void main() {
+
+        System.out.println("I am learning Java Backend");
+
+        // Difference between print and println
+        System.out.print("This is line A. ");
+        System.out.print("This is line B.");
+        System.out.println(" This is line C (still on same line?)");
+        System.out.println("Now we are on a new line.");
+
+        // Task
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("Java is cool!");
+    }
+}
